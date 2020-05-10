2413e93e9a5f094745b70aecffef825fe8b4ad54
Yevhenii Yukhymchuk <ievgenii.iukhymchuk@gmail.com>
2020-05-10 13:21:11 +0300

    Refactor

--- SetTheoryExpressionSimplification/Pipe/PatternMatcher.cs
+++ SetTheoryExpressionSimplification/Pipe/PatternMatcher.cs
@@ -16,28 +16,30 @@ namespace SetTheory
         {
             foreach (var rule in rules)
             {
-                var result = expr.DFSPostOrder(x => ApplyRule(x, rule));
-                if (result.HasValue)
+                var substitutions = new List<Substitution>();
+                expr.DFSPostOrder(x => ApplyRule(x, rule, substitutions));
+                if (substitutions.Any())
                 {
-                    result.Value.Expression = ChangeTree(expr, result.Value.Initial, result.Value.Resulting);
-                    return result;
+                    var substitution = substitutions.First();
+                    substitution.Expression = Substitute(expr, substitution.Initial, substitution.Resulting);
+                    return new Result<Substitution>(substitution);
                 }
             }
             return Result<Substitution>.Empty();
         }
 
-        static Result<Substitution> ApplyRule(Expression current, Rule rule)
+        static void ApplyRule(Expression current, Rule rule, List<Substitution> substitutions)
         {
-            var substitutions = new Dictionary<string, Expression>();
+            var substitutionsDictionary = new Dictionary<string, Expression>();
 
-            var doMatch = Match(current, rule.PatternIn, substitutions);
+            var doMatch = Match(current, rule.PatternIn, substitutionsDictionary);
             if (!doMatch)
-                return Result<Substitution>.Empty();
+                return;
 
             var patternOutCopy = rule.PatternOut.Copy();
-            var resulting = Substitute(patternOutCopy, substitutions);
+            var resulting = Substitute(patternOutCopy, substitutionsDictionary);
 
-            return new Result<Substitution>(
+            substitutions.Add(
                 new Substitution
                 {
                     Initial = current,
@@ -46,12 +48,45 @@ namespace SetTheory
                 });
         }
 
-        static Expression ChangeTree(Expression expr, Expression initial, Expression resulting)
+        static Expression Substitute(Expression expr, Expression initial, Expression resulting)
         {
             var tree = expr.Copy();
             tree.DFSPostOrder(
                 x => x.Children = x.Children.Select(y => Expression.ExprEquals(y, initial) ? resulting : y).ToArray());
             return tree;
+        }
+
+        static bool Match2(
+            Expression expr,
+            Expression pattern,
+            Dictionary<string, Expression> substitutions)
+        {
+            if (IsVariable(pattern))
+            {
+                var exists = substitutions.TryGetValue(pattern.Value, out var substitution);
+                if (exists)
+                    return Expression.ExprEquals(expr, substitution);
+
+                substitutions[pattern.Value] = expr.Copy();
+                return true;
+            }
+
+            if (expr.Type != pattern.Type) return false;
+            if (expr.Value != pattern.Value) return false;
+
+            if (IsOperation(expr) && IsOperation(pattern))
+            {
+                //PartialMatch();
+            }
+
+            if (expr.Children.Length != pattern.Children.Length) return false;
+
+            foreach (var two in expr.Children.Zip(pattern.Children, (x, y) => (x, y)))
+            {
+                if (!Match(two.x, two.y, substitutions))
+                    return false;
+            }
+            return true;
         }
 
         static bool Match(
@@ -71,6 +106,12 @@ namespace SetTheory
 
             if (expr.Type != pattern.Type) return false;
             if (expr.Value != pattern.Value) return false;
+
+            if (IsOperation(expr) && IsOperation(pattern))
+            {
+                //PartialMatch();
+            }
+
             if (expr.Children.Length != pattern.Children.Length) return false;
 
             foreach (var two in expr.Children.Zip(pattern.Children, (x, y) => (x, y)))
@@ -90,5 +131,6 @@ namespace SetTheory
         }
 
         static bool IsVariable(Expression pattern) => pattern.Value.StartsWith("_");
+        static bool IsOperation(Expression expr) => expr.Type == typeof(Operation);
     }
 }
--- SetTheoryExpressionSimplification/Search.cs
+++ SetTheoryExpressionSimplification/Search.cs
@@ -6,10 +6,10 @@ namespace SetTheory
     public static class Search
     {
         public static void DFSPostOrder(this Expression current, Action<Expression> action)
-            => current.DFSPostOrderInternal(action, new HashSet<Expression>());
+            => DFSPostOrderInternal(current, action, new HashSet<Expression>());
 
         static void DFSPostOrderInternal(
-            this Expression current,
+            Expression current,
             Action<Expression> action,
             HashSet<Expression> visited)
         {
@@ -19,30 +19,9 @@ namespace SetTheory
             visited.Add(current);
 
             foreach (var child in current.Children)
-                child.DFSPostOrderInternal(action, visited);
+                DFSPostOrderInternal(child, action, visited);
 
             action?.Invoke(current);
-        }
-
-        public static Result<Substitution> DFSPostOrder(
-            this Expression current,
-            Func<Expression, Result<Substitution>> func,
-            HashSet<Expression> visited = null)
-        {
-            visited ??= new HashSet<Expression>();
-            if (visited.Contains(current))
-                return Result<Substitution>.Empty();
-
-            visited.Add(current);
-
-            foreach (var child in current.Children)
-            {
-                var childResult = child.DFSPostOrder(func, visited);
-                if (childResult.HasValue)
-                    return childResult;
-            }
-
-            return func.Invoke(current);
         }
     }
 }

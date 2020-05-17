7ed2327f8ad2f5e127732952a8ea40228ccaae0b
Yevhenii Yukhymchuk <ievgenii.iukhymchuk@gmail.com>
2020-05-18 00:01:15 +0300

    Rename web project to web client model

--- DiscreteMath.Web.ClientModel/Program.cs
+++ DiscreteMath.Web.ClientModel/Program.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace DiscreteMath.Web
+namespace DiscreteMath.Web.ClientModel
 {
     public class Program
     {
